a6fda83ac57f8ab865c7e1ad16b9345f90a10519
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2023-12-03 14:41:02 -0300

    comienzo de login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require("../models/user")
+
 const authController = {
     register: (req, res)=>{
         res.send("register")
@@ -6,9 +8,15 @@ const authController = {
 
     },
     login: (req, res)=>{
-        res.send("login")
+        res.render("auth/login")
     },
     postLogin: (req, res)=>{
+        const {email, password} = req.body
+        try{
+            const user = User.findOne({where: {email}})
+        }catch(e){
+            res.send("Error al iniciar sesion")
+        }
         res.send("post login")
     }
 }
