8828a3c853caaa64f6ba596350e46a04f2aa2c55
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2023-12-04 19:14:57 -0300

    crear y eliminar productos

--- src/controllers/admin/productController.js
+++ src/controllers/admin/productController.js
@@ -1,10 +1,11 @@
+const Producto = require("../../models/producto");
 const Product = require("../../models/producto");
 
 const productController = {
   index: async (req, res) => {
     const products = await Product.findAll();
     console.log(products)
-    res.render("inicio", { products: products });
+    res.render("admin/productos/index", { products: products });
     
   },
   create: async (req, res) => {
@@ -16,8 +17,21 @@ const productController = {
 
   getCreate: (req, res) => {
     console.log("Rendering create");
-    res.render("admin/create");
+    res.render("admin/productos/create");
   },
+  delete: async (req, res)=>{
+    const { id } = req.params
+    try{
+      await Producto.destroy({
+        where:{
+          id: id
+        }
+      })
+      res.redirect('/admin/products')
+    }catch(e){
+      res.send("no se pudo eliminar el producto")
+    }
+  }
 };
 
 module.exports = productController;
