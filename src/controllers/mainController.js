6d9b9c022cc34bb864db7d4ff1995564771391dd
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2023-12-12 19:18:57 -0300

    trayendo datos de db, fix header

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,16 +1,59 @@
+const Licencia = require("../models/licencia");
 const Producto = require("../models/producto")
 
 
 
 const mainController = {
+    index: (req, res)=>{
+        res.redirect('/home')
+    },
+
     home: async (req, res)=> {
         try{
-            const products = await Producto.findAll()
-            res.render("inicio", {products: products});
+            const products = await Producto.findAll({
+                include: {
+                    model: Licencia,
+                }
+            })
+            res.render("index", {products: products});
         }catch(e){
             console.log(e)
             res.sendStatus(500).send(e)
         }
+    },
+
+    shop: async (req, res)=>{
+        try{
+            const products = await Producto.findAll({
+                include: {
+                    model: Licencia,
+                }
+            })
+            res.render("public/shop", {products: products});
+        }catch(e){
+            console.log(e)
+            res.sendStatus(500).send(e)
+        }
+    },
+
+    item: async (req, res)=>{
+        let {id} = req.params;
+        id = Number(id)
+        try{
+            const products = await Producto.findAll({
+                include:{
+                    model: Licencia
+                }
+            })
+            const product = products.find(producto=> producto.id === id);
+            console.log(product, products)
+            res.render("public/item", {product: product, products: products})
+
+        }catch(e){
+            console.log(e)
+            res.sendStatus(500).send(e)
+        }
+
     },
 
 
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,9 +2,12 @@ const express = require("express")
 const mainController = require("../controllers/mainController")
 const router = express.Router()
 
+router.get('/', mainController.index)
 router.get('/home', mainController.home)
 router.get('/contact', mainController.contact)
 router.get('/about', mainController.about)
 router.get('/faqs', mainController.faqs)
+router.get('/shop', mainController.shop)
+router.get('/item/:id', mainController.item)
 
 module.exports = router
