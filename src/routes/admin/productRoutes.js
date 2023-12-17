6e22a38d326a5beec724242b66904e4892d6e073
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2023-12-17 12:34:22 -0300

    validacion creacion productos

--- src/routes/admin/productRoutes.js
+++ src/routes/admin/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const productController = require('../../controllers/admin/productController')
 const multer = require("multer")
+const { formValidations } = require('../../middlewares/validacionForm')
 const upload = multer({ storage: multer.memoryStorage()})
 
 const router = express.Router()
@@ -9,10 +10,10 @@ const router = express.Router()
 router.get('/', productController.index)
 
 router.get('/create', productController.getCreate)
-router.post('/create', upload.array('imagenes', 2), productController.create)
+router.post('/create', upload.array('imagenes', 2),formValidations, productController.create)
 
 router.get('/edit/:id', productController.getEdit)
-router.put('/:id', upload.array('imagenes', 2), productController.edit)
+router.put('/:id', formValidations, upload.array('imagenes', 2), productController.edit)
 
 router.delete('/:id', productController.delete)
 
